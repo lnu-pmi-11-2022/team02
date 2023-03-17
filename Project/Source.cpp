d42bee7fd321c352a97f7cbf0a0663100e952203
Volodymyr Fedynjak <85686566+Volplayed@users.noreply.github.com>
2023-03-17 17:08:24 +0200

    created project files

---
+++ Project/Source.cpp
@@ -0,0 +1,24 @@
+//FILE COPIES FINDER
+//Team 02 - Irredeemable
+//Members:
+//Fedyniak Volodymyr
+//Kvasnytsia Uliana
+//Yakymets Danylo
+//Mularchyk Bohdan
+//Fedorniak Serhii
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+
+using namespace std;
+
+int main() {
+
+	cout << "Irredeemable - the best team ever!!!" << endl;
+
+	return 1;
+}
