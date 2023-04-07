b939c9bb155363855f10faf770eee6bf481da293
YakDanylo <alatp574@gmail.com>
2023-04-07 10:55:30 +0300

    added setCopy isCopy

--- Project/file.h
+++ Project/file.h
@@ -84,6 +84,14 @@ public:
    
    
 }
+void setCopy()
+   {
+    isCopy= true;
+   }
+   bool getIsCopy()
+   {
+    return isCopy;
+   }
     //checks file type
     bool isOfType(string fileType) {
         string temp = "";
@@ -106,5 +114,5 @@ public:
 
 private:
     string filePath;
-    bool isCopy=false;
+    bool isCopy = false;
 };
